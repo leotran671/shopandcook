0fe79ad4064e8092c375a7d8ab9314036860b049
leotran671 <leo.tran@my-digital-school.org>
2023-06-19 17:31:29 +0200

    change home responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExplanationComponent } from './explanation/explanation.component';
+import { ApplicationComponent } from './application/application.component';
+import { TradeComponent } from './trade/trade.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExplanationComponent,
+    ApplicationComponent,
+    TradeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
